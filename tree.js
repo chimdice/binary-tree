0d98c874ac617ab59d5d05ea730a878a0c4c3285
chimdice <118031439+chimdice@users.noreply.github.com>
2024-06-21 19:31:04 -0700

    Create tree class

---
+++ tree.js
@@ -0,0 +1,60 @@
+class Node {
+    constructor () {
+        this.value = null;
+        this.left = null;
+        this.right = null;
+    };
+};
+
+class Tree {
+    constructor (array) {
+        this.array = [...new Set(array.sort())];
+        
+        this.root = this.buildTree(this.array);
+    };
+
+    buildTree (array) {
+        let start = 0;
+        let end = array.length;
+        let mid = Math.round((start+end)/2);
+
+
+        if (end === 0){
+            return null;
+        } else {
+            const rootNode = new Node();
+            rootNode.value = array[mid];
+            const root = rootNode;
+
+            if (end === 1) {
+                return root;
+            } else {
+                const leftNode = this.buildTree(array.slice(start, mid));
+                const rightNode = this.buildTree(array.slice(mid+1, end));
+
+                root.right = rightNode
+                root.left = leftNode;
+                return root
+            }
+
+           t
+        };
+    };
+};
+
+const tree = new Tree([2,5,9,1,7]);
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+ 
+  prettyPrint(tree.root)
