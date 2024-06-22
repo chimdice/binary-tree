69730a5bf7c2018791cf635dee65ad51864199f3
chimdice <118031439+chimdice@users.noreply.github.com>
2024-06-21 20:34:02 -0700

    Add build tree method which constructs the tree

--- tree.js
+++ tree.js
@@ -8,7 +8,7 @@ class Node {
 
 class Tree {
     constructor (array) {
-        this.array = [...new Set(array.sort())];
+        this.array = [... new Set(array.sort((a, b) => a - b))];
         
         this.root = this.buildTree(this.array);
     };
@@ -16,7 +16,7 @@ class Tree {
     buildTree (array) {
         let start = 0;
         let end = array.length;
-        let mid = Math.round((start+end)/2);
+        let mid = Math.floor((start+end)/2);
 
 
         if (end === 0){
@@ -32,17 +32,19 @@ class Tree {
                 const leftNode = this.buildTree(array.slice(start, mid));
                 const rightNode = this.buildTree(array.slice(mid+1, end));
 
-                root.right = rightNode
+                root.right = rightNode;
                 root.left = leftNode;
                 return root
-            }
+            };
 
-           t
         };
     };
+
+
+    
 };
 
-const tree = new Tree([2,5,9,1,7]);
+const tree = new Tree([1, 9, 0]);
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
     if (node === null) {
@@ -57,4 +59,4 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     }
   };
  
-  prettyPrint(tree.root)
+prettyPrint(tree.root)
